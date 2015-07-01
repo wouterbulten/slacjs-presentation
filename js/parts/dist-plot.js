6b099a0f1df52cb0c8e884fbc2826fd31ff8d5ca
Wouter Bulten <wouterbulten@mai-ai.org>
2015-07-01 20:45:47 +0200

    Updated the two rssi plots

--- js/parts/dist-plot.js
+++ js/parts/dist-plot.js
@@ -2,47 +2,32 @@ var dist = Y.map(function(rssi) {
 	return Math.pow(10, (rssi + 62) / (-10 * 2));
 });
 
-var sum = dist.map(function(d) {
-	return Math.abs(d - 1);
-})
-.reduce(function(total, d) {
-	return total + d;
-});
-
-Reveal.addEventListener('dist-plot', function() {
-	$('#dist-chart').highcharts({
-        title: {
-            text: 'Predicted distance',
-            x: -20 //center
-        },
-        yAxis: {
-            title: {
-                text: 'Distance in meters'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#be311a'
-            }]
-        },
-		chart: {
-			animation: {
-				duration: 20000
-			}
-		},
-		legend: {
-			enabled: false
-		},
-        tooltip: {
-            valueSuffix: 'm'
-        },
-        series: [{
-            name: 'Predicted distance at 100cm',
-            data: dist
-        },
+var distData = {
+	labels: dist.map(function(y, i) {
+		return i;
+	}),
+	datasets: [
 		{
-			name: 'True distance',
-			data: [[0,1], [330, 1]]
-		}]
-    });
+			label: 'RSSI measurements at 1m',
+			fillColor: 'rgba(220,220,220,0.2)',
+			strokeColor: '#A3BBBF',
+			pointColor: '#1AA7BE',
+			pointStrokeColor: '#fff',
+			pointHighlightFill: '#fff',
+			pointHighlightStroke: 'rgba(220,220,220,1)',
+			data: dist
+		}
+	]
+};
+
+Reveal.addEventListener('ready', function() {
+
+	var ctx = $('#chart-dist').get(0).getContext('2d');
+	var line = new Chart(ctx).Line(distData, {
+		scaleShowGridLines: true,
+		scaleShowHorizontalLines: true,
+		scaleShowVerticalLines: false,
+		datasetStrokeWidth: 5,
+		pointDotRadius: 6
+	});
 });
